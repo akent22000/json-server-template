2f8e2fd295c7a2e931497b1f4df4ae9557545a06
amandalocher <akent22000@gmail.com>
2024-04-28 17:34:52 -0400

    added some pseudocode

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 
-document.addEventListener('DOMContentLoaded', () => displayCookies());
+//As a user I want to visit the web app and see a list of six Cookie Run Kingdom characters when the page loads.
+//In this list, I want to see each Cookies character name, rarity, battle position, battle type, and an image of each Cookie.
+
+// document.addEventListener('DOMContentLoaded', () => displayCookies());
+
+//Speaking in technical terms about asynchronous JS and fetch
+//Server communication (how do you make request, what's returned by FETCH, what's the difference between async (asynchronous) & synchronous code)
+//ES6 syntax (destructuring, arrow functions etc.) and how you're using it
+//Variable declarations, logical operators, callback functions, scope
+//Iterators (forEach, map, filter etc)
+//The DOM & how to manipulate the DOM (adding, updating, grabbing content, event listeners)
+
+
+// The differences between asynchronous and synchronous include: Async is multi-thread, which means operations or programs can run in parallel. Sync is a single-thread, so only one operation or program will run at a time. Async is non-blocking, which means it will send multiple requests to a server
 
 // Function to fetch data from the API
+// Using async here since I opted for the try, await, and catch 
+// Fetch is async by default
 async function fetchCookies() {
     try {
         const response = await fetch('http://localhost:3000/cookies');
         const cookies = await response.json();
+        // console.log(cookies)
+
         return cookies;
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
+// Display JSON data on the HTML page
 
-async function displayCookies() {
+// Grab cookie data from fetch function
+// Map array data to point to each data object
+// Objects to display = name, image, rarity, position, and type
+// Create each html element 
+// Declare each html element
+// Nest each html element
+
+let displayCookies = async () => {
     const cookies = await fetchCookies();
 
-    console.log(cookies)
     const cardsContainer = document.getElementById('display');
 
-    cookies.map(cookie => {
+    cookies.forEach(cookie => {
+
         const div = document.createElement('div');
         const image = document.createElement('img');
         const name = document.createElement('h3');
@@ -48,11 +73,14 @@ async function displayCookies() {
         // div.appendChild(like)
         cardsContainer.appendChild(div)
     });
-}
+};
 
+displayCookies();
+//As a user I want to check the checkbox on the page, then see the Cookies displayed in alphabetical order by name.
 
-function createCheckbox() {
-
+// Create checkbox html element and render it to the html page
+// Add id to link to html and css
+let createCheckbox = () => {
     let containerCheckbox = document.getElementById('containerCheckbox');
 
     let checkboxDiv = document.createElement("div");
@@ -71,44 +99,62 @@ function createCheckbox() {
 }
 createCheckbox();
 
-async function sortCookies() {
-    const cookies = await fetchCookies();
 
+//As a user I want to check the checkbox on the page, then see the Cookies displayed in alphabetical order by name.
+
+// Grab data from fetch function
+// Add an event listener to listen for the checkbox checked event and change when checked and unchecked
+// If checked do this... if uncheck do this...
+// Add if/else checked condition to see if checkbox has been checked or unchecked
+// itereate through cookie data using the array sort method alongside the compare function to order the array of cookie names.
+// in case json data is both upper and lower case, add something to ignore case
+
+// display sorted cookies on html page if checkbox is checked
+// display no cookies of checkbox is unchecked
+let sortCookies = async () => {
+    const cookies = await fetchCookies();
     console.log(cookies)
+
     let checkbox = document.getElementById("checkbox");
+
     checkbox.addEventListener("change", () => {
+
         if (checkbox.checked) {
             cookies.sort((a, b) => {
+                // a = The first element for comparison
+                // b = The second element for comparison
 
-                const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-                const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                const nameA = a.name.toUpperCase();
+                const nameB = b.name.toUpperCase();
+                // toUpperCase = ignore upper and lowercase
+
                 if (nameA < nameB) {
+                    // A negative value indicates that a should come before b.
                     return -1;
                 }
                 if (nameA > nameB) {
+                    // A positive value indicates that a should come after b.
                     return 1;
                 }
-                // names must be equal
+                // Zero or NaN indicates that a and b are considered equal.
                 return 0;
             });
+            displayed = "Yes"
+            console.log(cookies)
+            displayCookies();
 
-            // displayCookies(cookies);
+            // let text = ``;
+            // cookies.map(cookie => {
+            //     document.getElementById("display").innerHTML =
+            //         text += `
+            //         <div class="card">
+            //         <img src='${cookie.image}' class='image imgWrap' />
 
-            let text = ``;
-            cookies.map(cookie => {
-
-                console.log(cookies)
-
-                document.getElementById("display").innerHTML =
-                    text += `
-                    <div class="card">
-                    <img src='${cookie.image}' class='image imgWrap' />
-
-                    <h3>${cookie.name} "sort"</h3>
-                    <h3>${cookie.rarity}</h3>
-                    </div>`;
-                ;
-            });
+            //         <h3>${cookie.name} "sort"</h3>
+            //         <h3>${cookie.rarity}</h3>
+            //         </div>`;
+            //     ;
+            // });
 
         } else {
             document.getElementById("display").innerHTML = ''
@@ -117,8 +163,8 @@ async function sortCookies() {
 }
 sortCookies();
 
-
-async function searchCookies() {
+//As a user I want to search by Cookie names by typing the name into the search/input box.
+let searchCookies = async () => {
     const cookies = await fetchCookies();
 
     const charactersList = document.getElementById('charactersList');
@@ -126,6 +172,10 @@ async function searchCookies() {
 
     searchBar.addEventListener('keyup', (e) => {
         const searchString = e.target.value.toLowerCase();
+        // Define searchString and set value what the user is typing
+
+        // The filter() method of Array instances creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+        // Basic example = const result = words.filter((word) => word.length > 6);
 
         const filteredCharacters = cookies.filter((cookie) => {
             return (
@@ -159,18 +209,20 @@ searchCookies();
 
 
 
-// // JavaScript
-// // const likeButton = document.getElementById('like-button');
-// // const likeCountElement = document.getElementById('like-count');
+// // // JavaScript
+// // // const likeButton = document.getElementById('like-button');
+// // // const likeCountElement = document.getElementById('like-count');
 
-// //fetch character names
-// //like by character
-// //each character has like button
-// //each character has like counter
-// //when character like button clicked
-// //then update character like counter
+// // //fetch character names
+// // //like by character
+// // //each character has like button
+// // //each character has like counter
+// // //when character like button clicked
+// // //then update character like counter
 
-async function likeCookies() {
+// //As a user I want to see three battle position like buttons (Positions: Front, Middle, and Back). I want to click a like button, see the like counter increase by 1 and see the Cookies with the matching battle position.
+
+let likeCookies = async () => {
     const cookies = await fetchCookies();
 
     const charactersList1 = document.getElementById('like-count');
@@ -249,3 +301,52 @@ async function likeCookies() {
     };
 };
 likeCookies()
+
+
+
+
+// // var numbers = [1, 2, 3, 4, 5];
+// // let cookies = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+// let cookies = [
+//     {
+//         "name": "Ginger Brave",
+//         "rarity": "Common",
+//         "type": "Charge",
+//         "position": "Rear",
+//         "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/c/c1/Brave-cookie.png"
+//     },
+//     {
+//         "name": "Princess Cookie",
+//         "rarity": "Rare",
+//         "type": "Charge",
+//         "position": "Front",
+//         "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/4/48/Princess_cookie_alternate.png"
+//     }
+// ];
+
+
+
+
+// // Define the main function
+// function mainFunction(cookie) {
+//     // let cookies = fetchCookies();
+//     console.log("Performing operation...");
+//     // Use Array.forEach to loop through the array of numbers
+//     const cardsContainer = document.getElementById('display');
+
+//     cookies.forEach(cookie)
+
+
+//     console.log(cookies)
+
+// }
+
+// // Define the callback function
+// function callbackFunction(cookie) {
+//     console.log("Result: " + cookie.name);
+
+// }
+
+// // Call the main function with the callback function
+// mainFunction(callbackFunction);
+
